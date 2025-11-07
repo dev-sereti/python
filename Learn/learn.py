74a9e5419f434d975ae5e37e5f8b43ab7a85849b
dev-sereti <devsereti@gmail.com>
2025-11-07 10:13:22 +0300

    Data types.

---
+++ Learn/learn.py
@@ -0,0 +1,12 @@
+#Data types
+num=1
+num1=13.90
+num3=2+12j
+name='Sereti'
+isAged=True
+persons1=[19,32,53,"Sereti","John",True]
+persons2=(19,32,53,"Sereti","John",True)
+range1=(5)
+range2=(2,6)
+set1={1,2,3,4,5,6}
+set2=([1,2,3,4,5,6])
