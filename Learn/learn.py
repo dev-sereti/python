5a27385bcd74b06064b561ca1d3e7d36e191ca24
dev-sereti <devsereti@gmail.com>
2025-11-07 10:22:52 +0300

    Conditional Statements

--- Learn/learn.py
+++ Learn/learn.py
@@ -1,31 +1,7 @@
-#Data types
-num=1
-num1=13.90
-num2=2+12j
-name='Sereti'
-isAged=True
-persons1=[19,32,53,"Sereti","John",True]
-persons2=(19,32,53,"Sereti","John",True)
-range1=(5)
-range2=(2,6)
-set1={1,2,3,4,5,6}
-set2=([1,2,3,4,5,6])
-
-# Check data types
-print (type(num));
-print (type(num1));
-print (type(num2));
-print (type(name));
-print (type(isAged));
-print (type(persons1));
-print (type(persons2));
-print (type(range1));
-print (type(range2));
-print (type(set1));
-print (type(set2));
-
 #Conditional Statements
 #--- If statement ---
+
+
 
 
 
