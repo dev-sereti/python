44eb54b711d9b23d6ad60f8589fa40d1accb552c
dev-sereti <devsereti@gmail.com>
2025-11-07 10:36:41 +0300

    if-elif-else Statement

--- Learn/learn.py
+++ Learn/learn.py
@@ -7,7 +7,24 @@ if Age>=18:
 else:
     print("Under age")
 
+#if-elif-else Statement
 
+Score = 85.5
 
-
+if Score>=90:
+    print("Grade: A")
+elif Score>=80:
+    print("Grade: A-")
+elif Score>=70:
+    print("Grade: B+")
+elif Score>=60:
+    print("Grade: B")
+elif Score>=50:
+    print("Grade:B-")
+elif Score>=40:
+    print("Grade: C+")
+elif Score>=30:
+    print("Grade: C-")
+else:
+    print("Fail")
 
