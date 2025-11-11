da1630f284b0b683738505faac058a0b13b93410
dev-sereti <devsereti@gmail.com>
2025-11-11 08:01:01 +0300

    Lists

--- Learn/learn.py
+++ Learn/learn.py
@@ -1,11 +1,2 @@
-import time
-
-for i in range(5):
-    print(f"\rProgress: {i*20}%", end="")
-    time.sleep(0.5)
-print("\rComplete!       ") # Overwrite the last progress message
-# Output (on a single line, updating in place):
-# Progress: 0%
-# Progress: 20%
-# ...
-# Complete!
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
