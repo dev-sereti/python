af1c6c2545614e9d3618ba025dfd5975911d1896
dev-sereti <devsereti@gmail.com>
2025-11-11 09:53:47 +0300

    Join Two Lists

--- Learn/learn.py
+++ Learn/learn.py
@@ -1,13 +1,1 @@
-#Copy a List
-#Use the copy() method
-fruits = ["apple","Kiwi", "mango","banana", "cherry"]
-#newFruits = fruits.copy()
-
-
-#Use the list() method
-newFruits = list(fruits)
-#print(newFruits)
-
-#Use the slice Operator
-newFruits = fruits[:]
-print(newFruits)
+#Join Two Lists
