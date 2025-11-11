2d6a106ed46813e4df297d229ebf48055be9daa6
dev-sereti <devsereti@gmail.com>
2025-11-11 09:49:25 +0300

    Copy a List

--- Learn/learn.py
+++ Learn/learn.py
@@ -1,13 +1,5 @@
-#Sort List Alphanumerically
+#Copy a List
 
 fruits = ["apple","Kiwi", "mango","banana", "cherry"]
-
-fruits.sort()
-print(fruits)
-#Sort Descending
-fruits.sort(reverse=True)
-print(fruits)
-
-#Case Insensitive Sort
-fruits.sort(key=str.lower)
-print(fruits)
+newFruits = fruits.copy()
+print(newFruits)
