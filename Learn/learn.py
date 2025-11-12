87316e0f1d2584b858eb048dbc2f5f0ecce0e10e
dev-sereti <devsereti@gmail.com>
2025-11-12 09:57:53 +0300

    map function

--- Learn/learn.py
+++ Learn/learn.py
@@ -1,2 +1,12 @@
 #Python Recursion
 
+def even_numbers(num):
+    if num%2 == 1:
+        return num+1
+    else:
+        return num
+x = [44,56,53,22,123,45,67,8,89,87,56]
+
+y = list(map(even_numbers,x))
+print(y)
+    
