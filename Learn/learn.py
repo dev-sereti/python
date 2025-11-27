a96de8487e350c126fa0b26ebbd514433091c08d
dev-sereti <devsereti@gmail.com>
2025-11-27 14:36:43 +0300

    Python Else Statement

--- Learn/learn.py
+++ Learn/learn.py
@@ -1,10 +1,1 @@
-#Python Elif Statement
-
-number1 = 15
-number2 = 12
-
-if (number1>number2):
-    print("Value 1 is greater than 2")
-elif (number2 > number1):
-    print("Value 2 is greater than 1")
-    # comment: 
+#Python Else Statement
