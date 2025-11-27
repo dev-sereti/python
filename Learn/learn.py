c58cb38c0427fcfbf13692a6bf60604ab94dd29a
dev-sereti <devsereti@gmail.com>
2025-11-27 14:35:44 +0300

    Python Elif Statement

--- Learn/learn.py
+++ Learn/learn.py
@@ -1,5 +1,10 @@
 #Python Elif Statement
 
-number = 15
-if number > 0:
-  print("The number is positive")
+number1 = 15
+number2 = 12
+
+if (number1>number2):
+    print("Value 1 is greater than 2")
+elif (number2 > number1):
+    print("Value 2 is greater than 1")
+    # comment: 
