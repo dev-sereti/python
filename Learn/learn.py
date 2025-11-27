280005d642f5502cb06ceba0365dd5fec05a6d9c
dev-sereti <devsereti@gmail.com>
2025-11-27 15:20:51 +0300

    Python match Statement

--- Learn/learn.py
+++ Learn/learn.py
@@ -1,12 +1,2 @@
-#Python Nested If
+#Python Match Statement
 
-Age = 0
-has_Id = True
-
-if Age >= 18:
-    if has_Id:
-        print("You can vote")
-    else:
-        print("You need an Id")
-else:
-    print("You cannot vote")
