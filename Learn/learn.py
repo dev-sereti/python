229d549518c9939647bddf473b0198350641a331
dev-sereti <devsereti@gmail.com>
2025-11-27 15:00:02 +0300

    Python Else Statement

--- Learn/learn.py
+++ Learn/learn.py
@@ -1,10 +1,11 @@
 #Python Else Statement
 a = 200
-b = 300
+b = 200
 
-if (a<b):
+if a<b:
     print("a is less than b")
-elif (a>b):
-    print("a is greater than b")
-else:
+elif a==b:
     print("a and b are equal")
+else:
+    print("a is greater than b")
+    
