0256902649132b33bb39a993de6fe784c2469b5d
dev-sereti <devsereti@gmail.com>
2025-11-27 15:04:55 +0300

    Nested If

--- Learn/learn.py
+++ Learn/learn.py
@@ -1,11 +1,3 @@
-#Python Else Statement
-a = 200
-b = 200
+#Python Nested If
 
-if a<b:
-    print("a is less than b")
-elif a==b:
-    print("a and b are equal")
-else:
-    print("a is greater than b")
     
