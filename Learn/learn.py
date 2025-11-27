e815b17da0cb2bd706a63fa161d2d353c01b1352
dev-sereti <devsereti@gmail.com>
2025-11-27 15:16:21 +0300

    Nested If

--- Learn/learn.py
+++ Learn/learn.py
@@ -1,3 +1,12 @@
 #Python Nested If
 
-Age = 20
+Age = 0
+has_Id = True
+
+if Age >= 18:
+    if has_Id:
+        print("You can vote")
+    else:
+        print("You need an Id")
+else:
+    print("You cannot vote")
