64d176ae788d6a5110b38467eabb92c35ee14a55
dev-sereti <devsereti@gmail.com>
2025-11-28 07:55:32 +0300

    for loop in range()

--- Learn/learn.py
+++ Learn/learn.py
@@ -1,11 +1,4 @@
 #Python for Loops
 
-fruits = ["Oranges","Kiwi","Bananas","Apples"]
-
-for x in fruits:
-    if fruits == "Bananas":
-        break
-    print(x)
-    
-for y in "Bananas":
-    print(y)
+for x in range(6):
+    print (x)
