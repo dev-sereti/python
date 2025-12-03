17d5b3bda6e36303ac643886465facc2c22fdc3c
dev-sereti <devsereti@gmail.com>
2025-12-03 15:35:47 +0300

    Calculator

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -3,3 +3,21 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error! Division by zero."
+
+# Main program
+print("Welcome to Simple Calculator!")
+print("Select operation:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
